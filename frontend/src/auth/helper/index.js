072e704918bbbc2ac5bca6cf6cfd3016bd557657
Raj Joshi <joshiraj008@gmail.com>
2022-02-17 14:11:40 +0100

    Authentication Helper for React and some minor footer fixes

---
+++ frontend/src/auth/helper/index.js
@@ -0,0 +1,69 @@
+import { API } from "../../backend";
+
+export const signup = (user) => {
+  return fetch(`${API}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => console.log(err));
+};
+
+export const signup = (user) => {
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+// Sets our token into user browser
+export const authenticate = (data, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+// Removes the set token from the user browser
+export const signout = (next) => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+
+    return fetch(`${API}/signout`, {
+      method: "GET",
+    })
+      .then((response) => console.log("Sign Out Sucess"))
+      .catch((err) => console.log(err));
+  }
+};
+
+/** This method checks the authentication of user / simple boolean method
+ *
+ *  Access the window object
+ *  If we get a JWT token then user is authenticated else he is not authenticated */
+export const isAuthenticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};

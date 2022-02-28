ce853fef066d86147b257fbd586b0d5b267b1386
Raj Joshi <joshiraj008@gmail.com>
2022-02-28 21:37:46 +0100

    Added category routes

---
+++ frontend/src/admin/AddCategory.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Base from "../core/Base";
+
+export default function AddCategory() {
+  return (
+    <Base
+      title="Create a category"
+      description="Add a new category for T-shirts"
+      className="container bg-info p-4"
+    >
+      <div className="row bg-white rounded">
+        <div className="col-md-8 offset md-2">
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </Base>
+  );
+}

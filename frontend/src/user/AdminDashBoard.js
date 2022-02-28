6182f47f9c7ce5bd181c41bebdbc77f23aa11da5
Raj Joshi <joshiraj008@gmail.com>
2022-02-28 17:46:14 +0100

    Admin Right Side

--- frontend/src/user/AdminDashBoard.js
+++ frontend/src/user/AdminDashBoard.js
@@ -14,16 +14,24 @@ const AdminDashBoard = () => {
         <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
         <ul className="list-group">
           <li className="list-group-item">
-            <Link to="/admin/create/category" className="nav-link text-success">Create Categories</Link>
+            <Link to="/admin/create/category" className="nav-link text-success">
+              Create Categories
+            </Link>
           </li>
           <li className="list-group-item">
-            <Link to="/admin/create/product" className="nav-link text-success">Create Product</Link>
+            <Link to="/admin/create/product" className="nav-link text-success">
+              Create Product
+            </Link>
           </li>
           <li className="list-group-item">
-            <Link to="/admin/products" className="nav-link text-success">Manage Products</Link>
+            <Link to="/admin/products" className="nav-link text-success">
+              Manage Products
+            </Link>
           </li>
           <li className="list-group-item">
-            <Link to="/admin/orders" className="nav-link text-success">Manage Orders</Link>
+            <Link to="/admin/orders" className="nav-link text-success">
+              Manage Orders
+            </Link>
           </li>
         </ul>
       </div>
@@ -33,9 +41,22 @@ const AdminDashBoard = () => {
   const adminRightSide = () => {
     return (
       <div>
-        <h1>Hi Admin</h1>
+        <div className="card mb-4">
+          <h4 className="card-header">Admin Information</h4>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <span className="badge badge-success mr-2">Name: </span> {name}
+            </li>
+            <li className="list-group-item">
+              <span className="badge badge-success mr-2">E-mail:</span>{email}
+            </li>
+            <li className="list-group-item">
+              <span className="badge badge-danger">Admin Area</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    )
+    );
   };
 
   return (
@@ -46,7 +67,7 @@ const AdminDashBoard = () => {
     >
       <div className="row">
         <div className="col-3">{adminLeftSide()}</div>
-        <div className="col-3">{adminRightSide()}</div>
+        <div className="col-9">{adminRightSide()}</div>
       </div>
     </Base>
   );

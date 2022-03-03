b2f6203a39e3d4d642626c5efc26d8646c39c698
Raj Joshi <joshiraj008@gmail.com>
2022-03-03 10:08:22 +0100

    Created a reusable Card for Cart Page.

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from "react";
 import "../styles.css";
-import Base from './Base';
-
+import Base from "./Base";
+import Cart from "./Cart";
 
 export default function Home() {
   return (
-    <Base title='Home Page' description='Welcome to the eCommerce App'>
-        {/* <div className="row">
-          <div className="col-4">
-            <button className="btn btn-success">TEST</button>
-          </div>
-          <div className="col-4">
-            <button className="btn btn-success">TEST</button>
-          </div>
-          <div className="col-4">
-            <button className="btn btn-success">TEST</button>
-          </div>
-        </div> */}
+    <Base title="Home Page" description="Welcome to the eCommerce App">
+      <div className="row text-center">
+        <div className="col-4">
+          <button className="btn">
+            <Cart />
+          </button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+      </div>
     </Base>
-  )
+  );
 }
